f9899be1c3fc2d5eea93d2a693338c876c59f071
C Rainbow <c.rainbow.678@gmail.com>
2019-03-09 01:24:22 -0800

    More methods in base_repository.go

--- base_repository.go
+++ base_repository.go
@@ -10,6 +10,11 @@ import (
 	"github.com/guregu/dynamo"
 )
 
+const (
+	botTableName     = "Bots"
+	channelTableName = "Channels"
+)
+
 var DatabaseEndpoint = *flag.String("dynamodb-endpoint", "http://localhost:8000", "DynamoDB endpoint address")
 var DatabaseRegion = *flag.String("dynamodb-region", "us-west-2", "Default Region for DynamoDB")
 var DisableSSL = *flag.Bool("dynamodb-disable-ssl", true, "If true, disable SSL to connect to DynamoDB")
@@ -46,7 +51,7 @@ func NewBaseRepository() *BaseRepository {
 // GetAllBots returns all Bot models in the database.
 func (repo *BaseRepository) GetAllBots() []*models.Bot {
 	bots := []*models.Bot{}
-	err := repo.db.Table("Bots").Scan().All(&bots)
+	err := repo.db.Table(botTableName).Scan().All(&bots)
 	if err != nil {
 		log.Fatal("Error while fetching all bots", err.Error())
 	}
@@ -56,7 +61,7 @@ func (repo *BaseRepository) GetAllBots() []*models.Bot {
 // GetAllChannels returns all Channel models in the database.
 func (repo *BaseRepository) GetAllChannels() []*models.Channel {
 	channels := []*models.Channel{}
-	err := repo.db.Table("Channels").Scan().All(&channels)
+	err := repo.db.Table(channelTableName).Scan().All(&channels)
 	if err != nil {
 		log.Fatal("Error while fetching all channels", err.Error())
 	}
@@ -75,4 +80,36 @@ func (repo *BaseRepository) GetCommands(botID int64, channel string) map[string]
 func (repo *BaseRepository) GetCommand(botID int64, channel string, commandName string) *models.Command {
 	key := CommandMapKey{botID, channel}
 	return repo.commandMap[key][commandName]
+}
+
+func (repo *BaseRepository) CreateNewBot(botInfo *models.Bot) error {
+	botTable := repo.db.Table(botTableName)
+	err := botTable.Put(botInfo).Run()
+	return err
+}
+
+func (repo *BaseRepository) AddBotToChannel(botInfo *models.Bot, channel *models.Channel) error {
+	// Procedures
+	// 1. Check the bot ID is correct
+	// 2. Check the channel ID is correct
+
+	// 3. Check if the bot already runs in the channel
+	// TODO: Make sure that "channel" is up-to-date.
+	botExists := false
+	for _, botID := range channel.BotIDs {
+		if botID == botInfo.TwitchID {
+			botExists = true
+			break
+		}
+	}
+	// 4. Add the botID to the channel
+	if !botExists {
+		chanTable := repo.db.Table(channelTableName)
+		channel.BotIDs = append(channel.BotIDs, botInfo.TwitchID)
+		err := chanTable.Put(channel).Run()
+		return err
+	}
+	return nil
+
+	// 5. (Outside of this file) Bot joins the channel
 }
