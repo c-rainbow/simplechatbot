3244e5ed3e0442d04152df113178a0cb2f9c1333
c-rainbow <c.rainbow.678@gmail.com>
2020-03-09 00:05:09 -0700

    First draft translation to American English

--- localization/americanenglish/command.go
+++ localization/americanenglish/command.go
@@ -4,37 +4,37 @@ import "github.com/c-rainbow/simplechatbot/localization/common"
 
 var (
 	addCommandMessages = common.AddCommandMessages{
-		DefaultSuccess:             "@$(user) 새 명령어를 추가하였습니다",
-		DefaultFailure:             "실패하였습니다",
-		NoPermission:               "@$(user) 추가 권한이 없습니다",
-		NotEnoughArgument:          "입력 형식이 잘못되었습니다",
-		TargetCommandAlreadyExists: "같은 명령어가 이미 있습니다",
+		DefaultSuccess:             "@$(user) successfully added the new command",
+		DefaultFailure:             "Failed to add the new command",
+		NoPermission:               "@$(user) You don't have permission to add command",
+		NotEnoughArgument:          "Invalid format",
+		TargetCommandAlreadyExists: "The command already exists",
 	}
 
 	editCommandMessages = common.EditCommandMessages{
-		DefaultSuccess:         "@$(user) 명령어를 수정하였습니다",
-		DefaultFailure:         "명령어를 수정하지 못했습니다",
-		NoPermission:           "@$(user) 수정 권한이 없습니다",
-		NotEnoughArgument:      "입력 형식이 잘못되었습니다",
-		TargetCommandNotExists: "수정할 명령어가 존재하지 않습니다",
+		DefaultSuccess:         "@$(user) Successfully edited the command",
+		DefaultFailure:         "Failed to edit the command",
+		NoPermission:           "@$(user) You don't have permission to edit command",
+		NotEnoughArgument:      "Invalid format",
+		TargetCommandNotExists: "Command does not exist",
 	}
 
 	deleteCommandMessages = common.DeleteCommandMessages{
-		DefaultSuccess:         "@$(user) 명령어를 삭제하였습니다",
-		DefaultFailure:         "명령어를 삭제하지 못했습니다",
-		NoPermission:           "@$(user) 삭제 권한이 없습니다",
-		NotEnoughArgument:      "입력 형식이 잘못되었습니다",
-		TargetCommandNotExists: "삭제할 명령어가 존재하지 않습니다",
+		DefaultSuccess:         "@$(user) The command is now deleted",
+		DefaultFailure:         "Could not delete the command",
+		NoPermission:           "@$(user) You don't have permission to delete command",
+		NotEnoughArgument:      "Invalid format",
+		TargetCommandNotExists: "Command does not exist",
 	}
 
 	listCommandsMessages = common.ListCommandsMessages{
 		DefaultSuccess: "$(args0)",
-		DefaultFailure: "명령어를 가져올 수 없습니다",
+		DefaultFailure: "Cannot get the list of commands right now",
 	}
 
 	uptimeMessages = common.UptimeMessages{
-		DefaultOnline:  "$(uptime)동안 방송중입니다",
-		DefaultOffline: "지금은 방송중이 아닙니다",
+		DefaultOnline:  "Uptime: $(uptime)",
+		DefaultOffline: "The stream is offline",
 	}
 
 	botCommandLocale = common.BotCommandLocaleConfig{
--- localization/americanenglish/installer.go
+++ localization/americanenglish/installer.go
@@ -5,14 +5,14 @@ import "github.com/c-rainbow/simplechatbot/localization/common"
 var (
 	installerLocale = common.InstallerLocaleConfig{
 		Messages: common.InstallerMessages{
-			NoHelixClient:                "트위치 Helix API에 연결할 수 없습니다. Client ID를 확인해 주세요",
-			NoKrakenClient:               "트위치 Kraken API에 연결할 수 없습니다. Client ID를 확인해 주세요",
-			TwitchUsersAPIError:          "트위치 Helix Users API에 연결할 수 없습니다",
-			TwitchBotAccountNotFound:     "봇 트위치 계정을 찾을 수 없습니다",
-			TwitchChannelAccountNotFound: "채널을 찾을 수 없습니다",
-			ChatServerSuccessfulLogin:    "채팅 서버에 연결을 실패하였습니다",
-			ChatServerFailedLogin:        "채팅창 로그인에 실패하였습니다. OAuth 토큰을 확인해 주세요",
-			DynamoDBConnectionError:      "DynamoDB에 연결할 수 없습니다",
+			NoHelixClient:                "Cannot connect to Twitch Helix API. Please check ClientID",
+			NoKrakenClient:               "Cannot connect to Twitch Kraken API. Please check ClientID",
+			TwitchUsersAPIError:          "Cannot connect to Twitch Helix User API",
+			TwitchBotAccountNotFound:     "Cannot find the bot's Twitch account",
+			TwitchChannelAccountNotFound: "Cannot find the streamer's Twitch channel",
+			ChatServerSuccessfulLogin:    "Cannot connect to Twitch chat server",
+			ChatServerFailedLogin:        "Login to chat failed. Please check your OAuth token",
+			DynamoDBConnectionError:      "Cannot connect to DynamoDB",
 		},
 	}
 )
